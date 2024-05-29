6561ec169b787ca7efb9cf786b5ea02410577392
Adam <adam@imt-atlantique.net>
2024-05-29 12:19:29 +0200

    @dev:Ajout de l'export du script dans un zip

--- src/components/codeEditor.tsx
+++ src/components/codeEditor.tsx
@@ -1,19 +1,31 @@
 import CodeMirror from '@uiw/react-codemirror';
 import { StreamLanguage } from '@codemirror/language';
 import { shell } from '@codemirror/legacy-modes/mode/shell';
+<<<<<<< HEAD
 import {useState, useCallback, useEffect} from 'react';
+=======
+import { useState, useCallback } from 'react';
+>>>>>>> 08277ae (@dev:Ajout de l'export du script dans un zip)
 import { githubDark, githubLight } from '@uiw/codemirror-theme-github';
 import { useTheme } from '@/components/theme-provider';
 
 import { ScriptBuilder } from '@/utils/scriptBuilder';
 import { useFormContext } from '@/context/formContext';
 
+<<<<<<< HEAD
 import ZipGeneratorComponent from "@/context/zipGenerator.tsx";
+=======
+import { ScriptParser } from '@/utils/scriptParser';
+import { FormObject } from '@/context/formObject';
+import ZipGeneratorComponent from "@/context/zipGenerator.tsx";
+
+>>>>>>> 08277ae (@dev:Ajout de l'export du script dans un zip)
 const CodeEditor = () => {
     const {theme} = useTheme();
     const isDarkMode =
         theme === 'dark' || (theme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches);
 
+<<<<<<< HEAD
   const { formObject } = useFormContext();
   const [value, setValue] = useState('');
 
@@ -21,6 +33,16 @@ const CodeEditor = () => {
     const newValue = new ScriptBuilder(formObject).export();
     setValue(newValue);
   }, [formObject]);
+=======
+    const test : FormObject = ({
+      jar: [{ file: null, jvmArgs: '' }],
+      serverPath: '/home/test',
+      population: { file: [] },
+      logDir: '',
+      args: '',
+      sbatch: [{key: 'test', value: '1'}, {key: 'other', value: '2'}],
+  });
+>>>>>>> 08277ae (@dev:Ajout de l'export du script dans un zip)
 
   const onChange = useCallback((codeEditorValue: string) => {
     console.log('code editor:', codeEditorValue);
--- src/context/zipGenerator.tsx
+++ src/context/zipGenerator.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import JSZip from 'jszip';
+<<<<<<< HEAD
 import {Button} from '@/components/ui/button';
 import {TreeStructureEnum} from "@/utils/TreeStructureEnum.ts";
 import {useFormContext} from "@/context/formContext.tsx";
+=======
+import { Button } from '@/components/ui/button';
+import {TreeStructureEnum} from "@/utils/TreeStructureEnum.ts";
+>>>>>>> 08277ae (@dev:Ajout de l'export du script dans un zip)
 
 interface ZipGeneratorComponentProps {
     codeEditorValue: string;
 }
 
+<<<<<<< HEAD
 const {formObject} = useFormContext();
+=======
+>>>>>>> 08277ae (@dev:Ajout de l'export du script dans un zip)
 const ZipGeneratorComponent: React.FC<ZipGeneratorComponentProps> = ({ codeEditorValue }) => {
 
     const handleDownloadClick = () => {
         const zip = new JSZip();
 
+<<<<<<< HEAD
         const instancesFolder = zip.folder(TreeStructureEnum.INSTANCES);
 
         if (instancesFolder) {
@@ -38,6 +47,17 @@ const ZipGeneratorComponent: React.FC<ZipGeneratorComponentProps> = ({ codeEdito
 
         zip.file('script.sh', codeEditorValue);
         zip.generateAsync({type: 'blob'}).then((content) => {
+=======
+        for (const key in TreeStructureEnum) {
+            if (Object.prototype.hasOwnProperty.call(TreeStructureEnum, key)) {
+                const folderName = TreeStructureEnum[key as keyof typeof TreeStructureEnum];
+                zip.folder(folderName);
+            }
+        }
+
+        zip.file('script.sh', codeEditorValue);
+        zip.generateAsync({ type: 'blob' }).then((content) => {
+>>>>>>> 08277ae (@dev:Ajout de l'export du script dans un zip)
             const a = document.createElement('a');
             const url = URL.createObjectURL(content);
             a.href = url;
