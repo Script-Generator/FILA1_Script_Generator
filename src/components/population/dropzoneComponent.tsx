4bc173141a08e3fbf2f7de73b12aa1216a8803a7
Adam <adam@imt-atlantique.net>
2024-05-30 11:54:36 +0200

    @dev:Finalisation du zip

--- src/components/population/dropzoneComponent.tsx
+++ src/components/population/dropzoneComponent.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback } from 'react';
-import { useDropzone, DropzoneRootProps, DropzoneInputProps } from 'react-dropzone';
+import React, {useCallback} from 'react';
+import {useDropzone, DropzoneRootProps, DropzoneInputProps} from 'react-dropzone';
+import {FileFormatEnum} from "@/utils/fileFormatEnum.ts";
 
 interface DropZoneProps {
     onFileUpload: (file: File) => void;
@@ -7,23 +8,36 @@ interface DropZoneProps {
     onNameUpdate: (name: string) => void;
 }
 
-const DropZoneComponent: React.FC<DropZoneProps> = ({ onFileUpload, allowedExtension, onNameUpdate }) => {
+const DropZoneComponent: React.FC<DropZoneProps> = ({onFileUpload, allowedExtension, onNameUpdate}) => {
     const onDrop = useCallback((acceptedFiles: File[]) => {
         if (acceptedFiles.length === 1 && acceptedFiles[0].name.endsWith(allowedExtension)) {
             onFileUpload(acceptedFiles[0]);
         }
     }, [onFileUpload, allowedExtension, onNameUpdate]);
 
-    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    // need to return Accept type
+    const getAcceptedFileType = (extension: string): { [key: string]: string[] } => {
+        switch (extension) {
+            case FileFormatEnum.ZIP:
+                return {'application/zip': [allowedExtension]};
+            case FileFormatEnum.JAR:
+                return {'application/java-archive': [allowedExtension]};
+            default:
+                return {};
+        }
+    };
+
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({
         onDrop,
-        multiple: false
+        multiple: false,
+        accept: getAcceptedFileType(allowedExtension)
     });
 
     return (
         <div
             {...getRootProps() as DropzoneRootProps}
             className={`border-2 border-dashed p-4 rounded-md ${isDragActive ? 'border-blue-500' : 'border-gray-300'}`}>
-            <input {...getInputProps() as DropzoneInputProps} />
+            <input {...getInputProps() as DropzoneInputProps}/>
             {
                 isDragActive ?
                     <p className="text-center">Drop the {allowedExtension} file here...</p> :
