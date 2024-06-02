8cd9480c12faa79884e6265b0f5313cde6217069
tom <tom.freret@imt-atalntique.net>
2024-06-03 00:07:25 +0200

    @dev multi java file script builder

--- src/components/logOptions.tsx
+++ src/components/logOptions.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Input} from '@/components/ui/input.tsx';
-import {Label} from '@/components/ui/label.tsx';
-import {useFormContext} from '@/context/formContext.tsx';
+import { Input } from '@/components/ui/input.tsx';
+import { Label } from '@/components/ui/label.tsx';
+import { useFormContext } from '@/context/formContext.tsx';
 
 const LogOptions = () => {
   const { formObject, setFormObject } = useFormContext();
@@ -9,19 +9,23 @@ const LogOptions = () => {
   const handleLogOptionsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newLogOptions = event.target.value;
     setFormObject((prev) => {
-      return {...prev, logOptions: newLogOptions};
+      return { ...prev, logOptions: newLogOptions };
     });
   };
 
   return (
-    <div className="flex flex-col text-start gap-6">
+    <div className="flex flex-col text-start gap-6 mb-4">
       <div>
         <h2 className="text-xl font-bold">Log Options</h2>
         <Label htmlFor="serverPath">Choose way to construct your log folder</Label>
       </div>
-      <Input id="serverPath" type="text" placeholder="/home/cprudhom/"
-             value={formObject.logOptions}
-             onChange={handleLogOptionsChange}/>
+      <Input
+        id="serverPath"
+        type="text"
+        placeholder="/home/cprudhom/"
+        value={formObject.logOptions}
+        onChange={handleLogOptionsChange}
+      />
     </div>
   );
 };
